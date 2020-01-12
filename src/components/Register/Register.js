b41bf02ad82f360288d54e66f1bf0aad1f3dfe28
Ivan <ivan.rud.2012@mail.ru>
2020-01-12 20:10:57 +0300

    migrating to the server :p

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/register', {method: 'post', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
+        fetch('https://immense-earth-45257.herokuapp.com/register', {method: 'post', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({
             email: this.state.email,
             password: this.state.password,
             name: this.state.name
