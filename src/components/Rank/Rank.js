479dce07fbc0e74e1905c2aba235dfbac5b1c734
Kegbi <kegbi.networking@gmail.com>
2020-07-06 12:47:52 +0300

    Heroku stuff

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({name, entries}) => {
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: "",
+    };
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.name
+    ) {
+      return null;
+    } else {
+      this.generateEmoji(this.props.entries);
+    }
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    fetch(
+      `https://8ox1hni5xg.execute-api.us-east-1.amazonaws.com/prod/rank/create?rank=${this.props.entries}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ emoji: data.input }))
+      .catch(console.log);
+  };
+
+  render() {
     return (
-        <div>
-            <div className='white f3'>
-                {`${name}, your current entry count is...`}
-            </div>
-                <div className='white f1'>
-                    {entries}
-                </div>
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, your current entry count is...`}
         </div>
+        <div className="white f1">{this.props.entries}</div>
+        <div className="white f2">{`Rank badge: ${this.state.emoji}`}</div>
+      </div>
     );
+  }
 }
 
 export default Rank;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,15 +1,15 @@
 // Localhost
-export const SIGNIN = "http://localhost:30000/signin";
-export const REGISTER = "http://localhost:30000/register";
-export const IMAGEURL = "http://localhost:30000/imageurl";
-export const IMAGE = "http://localhost:30000/image";
-export const PROFILEUPD = "http://localhost:30000/profile/";
-export const LOGOUT = "http://localhost:30000/logout/";
+// export const SIGNIN = "http://localhost:30000/signin";
+// export const REGISTER = "http://localhost:30000/register";
+// export const IMAGEURL = "http://localhost:30000/imageurl";
+// export const IMAGE = "http://localhost:30000/image";
+// export const PROFILEUPD = "http://localhost:30000/profile/";
+// export const LOGOUT = "http://localhost:30000/logout/";
 
-// // Heroku server
-// export const SIGNIN = 'https://immense-earth-45257.herokuapp.com/signin';
-// export const REGISTER = 'https://immense-earth-45257.herokuapp.com/register';
-// export const IMAGEURL = 'https://immense-earth-45257.herokuapp.com/imageurl';
-// export const IMAGE = 'https://immense-earth-45257.herokuapp.com/image';
-// export const PROFILEUPD = "https://immense-earth-45257.herokuapp.com/profile/:";
-// export const LOGOUT = "https://immense-earth-45257.herokuapp.com/logout/";
+// Heroku server
+export const SIGNIN = "https://immense-earth-45257.herokuapp.com/signin";
+export const REGISTER = "https://immense-earth-45257.herokuapp.com/register";
+export const IMAGEURL = "https://immense-earth-45257.herokuapp.com/imageurl";
+export const IMAGE = "https://immense-earth-45257.herokuapp.com/image";
+export const PROFILEUPD = "https://immense-earth-45257.herokuapp.com/profile/:";
+export const LOGOUT = "https://immense-earth-45257.herokuapp.com/logout/";
